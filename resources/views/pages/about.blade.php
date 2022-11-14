40116c4c70bc17cc2f3f8699fbfb16fbeac9c0a2
Henry Ibrahima Vincent COLY <vincoly7@gmail.com>
2022-11-14 20:03:29 +0000

    Add route names and use them in views

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,11 @@
 @extends('app')
 
-@section('title', 'about us | '. config('project.variable'))
+@section('title', 'about us | '. config('app.name'))
+
 
 @section('content')
     
     <p>Built with &hearts; by LES TEACHERS DU NET</p>
 
-    <p><a href="/">Revenirvà la page d'acceuil</a></p>
+    <p><a href="{{ route('home') }}">Revenirvà la page d'acceuil</a></p>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     return view('pages/home');
-});
+})->name('home');
 
 
 
 Route::get('about-us', function() {
     return view('pages/about');
-});
+})->name('about');
